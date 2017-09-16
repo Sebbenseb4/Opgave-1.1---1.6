34ca0ef10c534d54ee989a921212d137770d0fc3
Sebbenseb4 <31951387+Sebbenseb4@users.noreply.github.com>
2017-09-16 23:26:42 +0200

    Add files via upload

---
+++ Opgave11.java
@@ -0,0 +1,61 @@
+package program1.example;
+
+import javax.xml.transform.Result;
+
+public class Opgave11 {
+
+    public static void main(String[] args) {
+
+//Opgave 1.1
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+        System.out.println(""); //Mellemrum
+/*
+//Opgave 1.2
+        //Print en kommentar x5 - Et loop
+        //Variabler
+        String LoveJava = "I love Java.";
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(LoveJava);
+        }
+        System.out.println(""); //Mellemrum
+//Opgave 1.4
+        System.out.println("a     a^2     a^3    a^4");
+        System.out.println("1     1       1      1");
+        System.out.println("2     4       8      16");
+        System.out.println("3     9       27     81");
+        System.out.println("4     16      64     256");
+        System.out.println(""); //Mellemrum
+
+//Opgave 1.5
+        System.out.println("7,5 x 6.5 - 4,5 x 3");
+        System.out.println("-------------------");
+        System.out.println("    47,5 - 5,5");
+        System.out.println(((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5));
+        System.out.println(""); //Mellemrum
+
+//Opgave 1.6
+            System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = "); //Mellemrum
+            System.out.print((1) + (2) + (3) + (4) + (5) + (6)+ (7) + (8) + (9) + (10));
+            System.out.println(""); //Mellemrum
+
+
+        /*
+        int a,b,c,d,e,f,g,h,i,j;
+
+        a=1;
+        b=2;
+        c=3;
+        d=4;
+        e=5;
+        f=6;
+        g=7;
+        h=8;
+        i=9;
+        j=10;
+        System.out.print(a+b+c+d+e+f+g+h+i+j);
+         */
+            }
+}

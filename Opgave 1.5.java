55eee63cbe097c32adad59dce23be91ff88fc9a1
Sebbenseb4 <31951387+Sebbenseb4@users.noreply.github.com>
2017-09-17 12:46:44 +0200

    Create Opgave 1.5.java

---
+++ Opgave 1.5.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+//Opgave 1.5
+
+        System.out.println("7,5 x 6.5 - 4,5 x 3");
+        System.out.print("------------------- = ");
+        System.out.println(((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5));
+        System.out.println("    47,5 - 5,5");
+
+    }
+}

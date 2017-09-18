a16ab64a592ce94e5d090b62f2d72046d4e45224
Sebbenseb4 <31951387+Sebbenseb4@users.noreply.github.com>
2017-09-18 18:56:10 +0200

    Update Opgave 1.4.java

--- Opgave 1.4.java
+++ Opgave 1.4.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println("a     a^2     a^3    a^4");
         System.out.println("1     1       1      1");
         System.out.println("2     4       8      16");
